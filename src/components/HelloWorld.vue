c0f92252a32b9d1da794f3f86774347a753fa9c3
Kyrylo-M <flyguardian3003@gmail.com>
2023-11-09 15:39:52 +0200

    Init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+
+    <p>
+      Name: Kyrylo Mytroshyn
+
+    </p>
+    <p>
+      Specialistation: 121
+    </p>
+    <p>
+      Group: 8.1212 -1-іпз
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
